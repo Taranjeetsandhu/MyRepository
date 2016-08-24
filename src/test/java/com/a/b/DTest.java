eac42003513e706f5e528e94a67aef9451daa229
Taranjeet Kaur <Taranjeet Kaur>
2016-08-24 12:28:45 +0530

    hello

---
+++ src/test/java/com/a/b/DTest.java
@@ -0,0 +1,27 @@
+package com.a.b;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DTest {
+
+	@Test
+	public void t1()
+	{
+		System.out.println("Hi I am test t1");
+		Assert.assertTrue(true, " ");
+	}
+	@Test
+	public void t2()
+	{
+		System.out.println("Hi I am test t2");
+		Assert.assertTrue(false, " ");
+	}
+	
+	@Test
+	public void t3()
+	{
+		System.out.println("Hi I am test t3");
+		Assert.assertTrue(false, " ");
+	}
+}
